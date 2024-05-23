445a2626ff27a0cfb25f55fac1d6b65913c7ddec
tushar26g <tushar26g@gmail.com>
2024-05-23 22:15:24 +0530

    added add post API

--- src/main/java/com/example/demo/Controller/postController.java
+++ src/main/java/com/example/demo/Controller/postController.java
@@ -4,9 +4,7 @@ import com.example.demo.entity.Post;
 import com.example.demo.repo.PostRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,11 @@ public class postController {
     public List<Post> getAllPosts()
     {
         return postRepo.findAll();
+    }
+
+    @PostMapping("/post")
+    public Post addPost(@RequestBody Post post)
+    {
+        return postRepo.save(post);
     }
 }
