3498e14d7bc4e534cb114064925d34d36c57686f
tushar26g <tushar26g@gmail.com>
2024-05-27 18:51:21 +0530

    fetchin webpage data

--- src/main/java/com/example/demo/Controller/SectionController.java
+++ src/main/java/com/example/demo/Controller/SectionController.java
@@ -2,26 +2,23 @@ package com.example.demo.Controller;
 
 import com.example.demo.entity.Section;
 import com.example.demo.repo.SectionRepository;
-import com.example.demo.service.SectionService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000") // Allow requests from your React app
+@RequestMapping("/section")
 public class SectionController {
-    @Autowired
-    private SectionService sectionService;
 
-    @GetMapping("/sections")
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @GetMapping
     public Section getSection(@RequestParam String key) {
-        System.out.println("Tushar");
-        return sectionService.getSectionByKey(key);
+        return sectionRepository.findByKey(key);
     }
 }
